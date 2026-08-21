cf47b0c97e19cee71b6926d9dd3d71d741243927
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-08-21 19:27:44 +0600

    Align tree with working RMX3191 reference
    Major fixes for boot failure:
    - BOARD_KERNEL_IMAGE_NAME: Image.gz -> kernel (match actual filename)
    - TARGET_ARCH_VARIANT: armv8-a -> armv8-2a (match RMX3191)
    - TARGET_CPU_VARIANT: cortex-a75 -> generic + cortex-a75 runtime
    - Added TARGET_2ND_CPU_VARIANT_RUNTIME: cortex-a75
    - Removed extra flags (TARGET_SUPPORTS_64_BIT_APPS, TARGET_IS_64_BIT, etc)
    - Removed BOARD_BUILD_SYSTEM_ROOT_IMAGE
    - Removed AVB vbmeta vendor settings (not in working tree)
    - Removed TW_NO_HAPTICS
    - Fixed fstab: double commas, added erofs entries, my_* partitions
    - Added vibrator libs from RMX3191
    - Added vibrator/keymaster HIDL modules
    - Added TW_SUPPORT_INPUT_AIDL_HAPTICS
    - Added PRODUCT_SHIPPING_API_LEVEL := 30
    - Matched fstab exactly with working RMX3191 reference

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,21 +22,21 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a75
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
-TARGET_SUPPORTS_64_BIT_APPS := true
-TARGET_IS_64_BIT := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := RM6768
@@ -51,7 +51,7 @@ TARGET_OTA_ASSERT_DEVICE := RMX3191,RMX3193,RMX3195,RMX3197,RMX3430,even
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_IMAGE_NAME := kernel
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -66,8 +66,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
 BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
+
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
@@ -89,10 +88,6 @@ BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
-BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 1
 
 # Dynamic Partitions
 BOARD_SUPER_PARTITION_SIZE := 10200547328
@@ -126,11 +121,11 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x8000000
 
 # System as root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+BOARD_SUPPRESS_SECURE_ERASE := true
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
-BOARD_ROOT_EXTRA_FOLDERS += metadata my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock my_version
+BOARD_ROOT_EXTRA_FOLDERS += metadata my_bigball my_carrier my_company my_engineering my_heytap my_manifest my_preload my_product my_region my_stock
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
@@ -145,6 +140,24 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TARGET_RECOVERY_DEVICE_MODULES := libinit_even
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+# Haptics
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client \
+    android.hardware.vibrator-V1-ndk_platform
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -169,7 +182,6 @@ TW_USE_TOOLBOX := true
 TW_OZIP_DECRYPT_KEY := 0000
 TW_Y_OFFSET := 50
 TW_H_OFFSET := -50
-TW_NO_HAPTICS := false
 TW_FRAMERATE := 60
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product"
